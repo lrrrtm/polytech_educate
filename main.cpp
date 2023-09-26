7516b2592481320ed81a4eb1140edaa98bc270cd
lrrrtm <lrrrtm.edu@yandex.ru>
2023-09-26 19:07:40 +0300

    рассмотрены случаи для отрицательной и положительной экспоненты

--- main.cpp
+++ main.cpp
@@ -1,9 +1,47 @@
 #include "iostream"
+#include "cmath"
 
 using namespace std;
 
+class Calculate {
+public:
+    int mantissa;
+    int exponent;
+
+    Calculate(double num){
+        int _exponent = 0;
+
+        if (num > 1) {
+            while (num > 1){
+                num /= 2;
+                _exponent++;
+            }
+
+        } else {
+            while (num < 0.1){
+                num *= 2;
+                _exponent--;
+
+            }
+        }
+
+        exponent = _exponent;
+        num *= 1000;
+        mantissa = (int) round(num);
+
+    }
+
+    void Display() {
+        cout << mantissa << " " << exponent << endl;
+    }
+};
+
 int main(){
-    cout << "Hello, world, Артём" << endl;
+    setlocale(LC_ALL, "RU");
+    Calculate _calculate(0.0152);
+    _calculate.Display();
 
     return 0;
 }
+
+
