2bc7c0e31643bb38d2715f9d3155db0f9190fc98
Artem Larionenko <74620760+lrrrtm@users.noreply.github.com>
2023-12-05 18:00:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "lrclassSat.h"
 #include "iostream"
+#include "vector.h"
 
 int main() {
-    lrclassSat s(223.4);
-    lrclassSat d(186.4);
+    /*
+    lrclassSat s(189.0);
+    lrclassSat d(186.0);
     lrclassSat res = s + d;
-
+    printf("lab 2\n\n");
     printf("Input data\n");
     s.print();
     d.print();
@@ -20,4 +22,24 @@ int main() {
     printf("\nmultiply\n");
     res = s * d;
     res.print();
+    */
+
+    vector b (3);
+    vector c (3);
+
+    for (int i = 0; i < 3; i++) {
+        b.elem(i) = lrclassSat(2.0);
+        c.elem(i) = lrclassSat(4.5);
+    }
+    printf("\nlab 3\n\nvector B\n");
+    for (int i = 0; i < 3; i++){
+        b.elem(i).print();
+    }
+    printf("vector C\n");
+    for (int i = 0; i < 3; i++){
+        c.elem(i).print();
+    }
+
+    vector result = b + c;
+
 }
